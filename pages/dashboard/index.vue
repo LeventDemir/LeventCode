47dac8a1a88eed60549bf127563cc0dcd82660e4
LeventDemir <leventdemir518@gmail.com>
2020-02-14 14:54:25 +0300

    dashboard page created

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div class="columns">
+      <div class="column is-full">
+        <button class="button is-success is-fullwidth">
+          <span class="icon is-small">
+            <i class="fas fa-plus"></i>
+          </span>
+          <span>Create New</span>
+        </button>
+      </div>
+    </div>
+
+    <br />
+    <br />
+
+    <div class="columns is-multiline is-centered">
+      <div v-for="i in 8" :key="i" class="column is-4">
+        <Card />
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Card from "@/components/card";
+
+export default {
+  components: { Card }
+};
+</script>
