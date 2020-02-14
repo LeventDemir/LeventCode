a994b90cbfb593248c444ab934c538072bf34715
LeventDemir <leventdemir518@gmail.com>
2020-02-14 14:23:26 +0300

    works page created

---
+++ pages/works.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="columns is-multiline is-centered">
+      <div v-for="i in 8" :key="i" class="column is-4">
+        <Card />
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Card from "@/components/card";
+
+export default {
+  components: { Card }
+};
+</script>
