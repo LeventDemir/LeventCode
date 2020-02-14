2d900302d64fbf60cf66958e370eecb32970a954
LeventDemir <leventdemir518@gmail.com>
2020-02-14 15:06:36 +0300

    product-form component, create and udate page created

---
+++ components/product-form.vue
@@ -0,0 +1,33 @@
+<template>
+  <form>
+    <div class="field">
+      <p class="control">
+        <input class="input" type="text" placeholder="Title" />
+      </p>
+    </div>
+
+    <div class="field">
+      <div class="control">
+        <textarea class="textarea" placeholder="Primary textarea"></textarea>
+      </div>
+    </div>
+
+    <input ref="photoInput" class="hide-file-input" type="file" accept="image/*" />
+
+    <button
+      @click="$refs.photoInput.click()"
+      class="button is-primary is-outlined is-fullwidth bold-border"
+      type="button"
+    >
+      <span class="icon is-small">
+        <i class="fas fa-camera"></i>
+      </span>
+      <span>Photo</span>
+    </button>
+
+    <br />
+
+    <button class="button is-success">Create</button>
+  </form>
+</template>
+
---
+++ pages/dashboard/create.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="columns">
+      <div class="column is-full">
+        <ProductForm />
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import ProductForm from "@/components/product-form";
+
+export default {
+  components: { ProductForm }
+};
+</script>
