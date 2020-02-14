c7505f5f13248568b0b2388f04802cea2ca1b428
LeventDemir <leventdemir518@gmail.com>
2020-02-14 13:26:43 +0300

    navbar component created

---
+++ components/navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-start">
+          <a class="navbar-item">Home</a>
+          <a class="navbar-item">About me</a>
+          <a class="navbar-item">Works</a>
+          <a class="navbar-item">Contact</a>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
