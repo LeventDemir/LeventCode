f2c27a83831c387e518496d606b5802e80bd89eb
LeventDemir <leventdemir518@gmail.com>
2020-02-17 15:25:51 +0300

    contact and about-me pages created

--- pages/about-me.vue
+++ pages/about-me.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="container">
     <div class="columns">
-      <div class="column is-full">
-        <p class="is-size-3">About me</p>
+      <div class="column is-8 is-offset-2">
+        <p class="is-size-3 has-text-centered">About me</p>
 
         <hr />
 
-        <p>Hello, my name is Levent. I'm a software developer, I'm interested in web applications, mobile applications and desktop applications. You can use my phone number or email to contact me.</p>
+        <p
+          class="has-text-centered"
+        >Hello, my name is Levent. I'm a software developer, I'm interested in web applications, mobile applications and desktop applications. You can use my phone number or email to contact me.</p>
 
         <br />
 
-        <p>
-          <span class="icon">
-            <i class="fas fa-phone"></i>
-          </span>
-          <span>+90 553 061 5830</span>
-        </p>
-        <p>
+        <p class="has-text-centered">
           <span class="icon">
             <i class="fas fa-envelope"></i>
           </span>
           <span>leventcoding@gmail.com</span>
+        </p>
+        <p class="has-text-centered">
+          <span class="icon">
+            <i class="fas fa-phone"></i>
+          </span>
+          <span>+90 553 061 5830</span>
         </p>
       </div>
     </div>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,27 +1,30 @@
 <template>
   <div class="container">
     <div class="columns">
-      <div class="column is-10 is-offset-1">
+      <div class="column is-full">
         <form>
           <div class="field">
+            <label class="label">Full Name</label>
             <p class="control has-icons-left">
-              <input class="input" type="text" placeholder="Full Name" />
+              <input class="input" type="text" />
               <span class="icon is-small is-left">
                 <i class="fas fa-user"></i>
               </span>
             </p>
           </div>
           <div class="field">
+            <label class="label">Email</label>
             <p class="control has-icons-left">
-              <input class="input" type="email" placeholder="Email" />
+              <input class="input" type="email" />
               <span class="icon is-small is-left">
                 <i class="fas fa-envelope"></i>
               </span>
             </p>
           </div>
           <div class="field">
+            <label class="label">Phone</label>
             <p class="control has-icons-left">
-              <input class="input" type="tel" placeholder="Phone" />
+              <input class="input" type="tel" />
               <span class="icon is-small is-left">
                 <i class="fas fa-phone"></i>
               </span>
@@ -29,12 +32,17 @@
           </div>
 
           <div class="field">
+            <label class="label">Message</label>
             <div class="control">
-              <textarea class="textarea" placeholder="Primary textarea"></textarea>
+              <textarea class="textarea"></textarea>
             </div>
           </div>
 
-          <button class="button is-success">Send</button>
+          <br />
+
+          <div class="has-text-centered">
+            <button class="button is-success">Send</button>
+          </div>
         </form>
       </div>
     </div>
