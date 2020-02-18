817f776e4121afc0cf52175c2560885dc6cbba3c
LeventDemir <leventdemir518@gmail.com>
2020-02-18 14:03:10 +0300

    bannter component updated

--- components/hero.vue
+++ components/hero.vue
@@ -6,8 +6,8 @@
 
     <div class="hero-body">
       <div class="container has-text-centered">
-        <h1 class="title has-text-white">Title</h1>
-        <h2 class="subtitle has-text-white">Subtitle</h2>
+        <h1 class="title has-text-white">Levent Code</h1>
+        <h2 class="subtitle has-text-grey-light">web & mobile developing</h2>
       </div>
     </div>
   </section>
